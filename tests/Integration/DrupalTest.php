61d47a87be5e8710704b2b0c11f325fcb1d6eee5
Eirik S. Morland <eirik@morland.no>
2019-02-16 19:47:51 +0100

    Add a test for Drupal recipe

---
+++ tests/Integration/DrupalTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Violinist\ComposerUpdater\Tests\Integration;
+
+class RegularTest extends IntegrationBase
+{
+    protected $package = 'drupal/core';
+    protected $directory = 'drupal-core';
+}
