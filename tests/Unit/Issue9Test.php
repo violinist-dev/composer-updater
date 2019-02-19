e226373d20bb012a87d576a96c4d4f4ff3aca581
Eirik S. Morland <eirik@morland.no>
2019-02-19 07:18:48 +0100

    Use a dummy process factory for unit tests. Fix #11

--- tests/Unit/Issue9Test.php
+++ tests/Unit/Issue9Test.php
@@ -3,6 +3,7 @@
 namespace Violinist\ComposerUpdater\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
 use Violinist\ComposerUpdater\Exception\NotUpdatedException;
 use Violinist\ComposerUpdater\Updater;
 
@@ -10,7 +11,11 @@ class UnitTest extends TestCase
 {
     public function testRegularUpdateNoComposerLock()
     {
+        $mock_process = $this->createMock(Process::class);
+        $factory = new DummyProcessFactory();
+        $factory->setProcess($mock_process);
         $updater = new Updater(__DIR__ . '/../assets/unit/issue9', 'harvesthq/chosen');
+        $updater->setProcessFactory($factory);
         $this->expectException(NotUpdatedException::class);
         $updater->executeUpdate();
     }
