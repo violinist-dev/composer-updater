223d0667fbc043f09ea67f78f7a02af6778b5522
Eirik S. Morland <eirik@morland.no>
2019-11-10 13:50:06 +0100

    Add a test for Drupal Commerce

---
+++ tests/Integration/DrupalCommerceTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Violinist\ComposerUpdater\Tests\Integration;
+
+class RegularTest extends IntegrationBase
+{
+    protected $package = 'drupal/commerce';
+    protected $directory = 'drupal-commerce';
+}
