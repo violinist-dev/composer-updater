9f08afbe058d218dafa8b84942bfd0c49fe86d4f
Eirik S. Morland <eirik@morland.no>
2020-07-28 21:19:20 +0200

    Add a test for league_oauth_login, and fix it

---
+++ tests/Integration/DrupalLeagueOauthLoginTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Violinist\ComposerUpdater\Tests\Integration;
+
+class DrupalLeagueOauthLoginTest extends IntegrationBase
+{
+    protected $package = 'drupal/league_oauth_login';
+    protected $directory = 'drupal-league_oauth_login';
+}
