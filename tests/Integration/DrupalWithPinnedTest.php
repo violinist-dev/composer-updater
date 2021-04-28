10b8a495ab1b8c22486d7a47da635407a39be918
Eirik S. Morland <eirik@morland.no>
2021-04-28 10:23:19 +0200

    Skip test for php 7.0

--- tests/Integration/DrupalWithPinnedTest.php
+++ tests/Integration/DrupalWithPinnedTest.php
@@ -6,4 +6,16 @@ class DrupalWithPinnedTest extends IntegrationBase
 {
     protected $package = 'drupal/core-recommended';
     protected $directory = 'drupal-core-with-pinned';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testEndToEnd()
+    {
+        if (version_compare(phpversion(), "7.1.0", "<=")) {
+            self::assertTrue(true, sprintf('Skipping integration test %s for version %s since PHP version is less than 7.1.0', get_class($this), phpversion()));
+            return;
+        }
+        parent::testEndToEnd();
+    }
 }
