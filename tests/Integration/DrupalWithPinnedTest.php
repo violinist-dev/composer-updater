7aa47dad0da80ba8a410be539de921e7b4e0bc9c
Eirik S. Morland <eirik@morland.no>
2021-04-28 07:53:29 +0200

    Add a test for drupal with drupal pinned dev

---
+++ tests/Integration/DrupalWithPinnedTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Violinist\ComposerUpdater\Tests\Integration;
+
+class DrupalWithPinnedTest extends IntegrationBase
+{
+    protected $package = 'drupal/core-recommended';
+    protected $directory = 'drupal-core-with-pinned';
+}
