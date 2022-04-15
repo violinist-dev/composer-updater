a045f4bd571ddee1c6ba3e0bf4d07d3b51ff4f7f
Eirik S. Morland <eirik@morland.no>
2022-04-15 18:57:54 +0200

    Refactor base test a little

--- tests/Integration/IntegrationBase.php
+++ tests/Integration/IntegrationBase.php
@@ -13,13 +13,34 @@ abstract class IntegrationBase extends TestCase
 
     protected $directory;
 
+    protected $lockString;
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        $file = $this->getFile();
+        // Now make sure we reset it after us.
+        @file_put_contents($file, $this->lockString);
+    }
+
+    protected function getFile()
+    {
+        $directory = $this->getDirectory();
+        return "$directory/composer.lock";
+    }
+
+    protected function getDirectory()
+    {
+        return __DIR__ . '/../assets/' . $this->directory;
+    }
+
     public function testEndToEnd()
     {
-        $directory = __DIR__ . '/../assets/' . $this->directory;
-        $file = "$directory/composer.lock";
-        $lock_string = @file_get_contents($file);
-        $pre_lock_data = ComposerLockData::createFromString($lock_string);
-        $updater = new Updater($directory, $this->package);
+        $file = $this->getFile();
+        $this->lockString = @file_get_contents($file);
+        $pre_lock_data = ComposerLockData::createFromString($this->lockString);
+        $directory = $this->getDirectory();
+        $updater = $this->createUpdater($directory, $this->package);
         try {
             $updater->executeUpdate();
         } catch (\Throwable $e) {
@@ -35,7 +56,11 @@ abstract class IntegrationBase extends TestCase
         $log_package_data_pre = $pre_lock_data->getPackageData($this->package);
         $log_package_data_post = $post_lock_data->getPackageData($this->package);
         $this->assertNotEquals($log_package_data_post->version, $log_package_data_pre->version);
-        // Now make sure we reset it after us.
-        @file_put_contents($file, $lock_string);
+    }
+
+    protected function createUpdater($directory)
+    {
+        $updater = new Updater($directory, $this->package);
+        return $updater;
     }
 }
