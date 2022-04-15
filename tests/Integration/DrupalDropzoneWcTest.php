78f3e5252a3763424dcedbeab90bbd1be44a00ad
Eirik Stanghelle Morland <eirik@morland.no>
2022-04-15 09:11:46 +0200

    Delete DrupalDropzoneWcTest.php

--- tests/Integration/DrupalDropzoneWcTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Violinist\ComposerUpdater\Tests\Integration;
-
-class DrupalDropzoneWcTest extends IntegrationBase
-{
-    protected $package = 'drupal/dropzonejs';
-    protected $directory = 'drupal-dropzonejs';
-}
