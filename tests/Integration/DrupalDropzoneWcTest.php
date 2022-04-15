c50527ae046f05958221f211d05ac67405641805
Eirik Stanghelle Morland <eirik@morland.no>
2022-04-15 08:48:53 +0200

    Create DrupalDropzoneWcTest.php

---
+++ tests/Integration/DrupalDropzoneWcTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Violinist\ComposerUpdater\Tests\Integration;
+
+class DrupalDropzoneWcTest extends IntegrationBase
+{
+    protected $package = 'drupal/dropzonejs';
+    protected $directory = 'drupal-dropzonejs';
+}
