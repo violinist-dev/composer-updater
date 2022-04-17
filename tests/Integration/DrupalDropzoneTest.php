f1ebf41043645a0047c6d331ce8c763744616cd7
Eirik Stanghelle Morland <eirik@morland.no>
2022-04-17 08:35:09 +0200

    Update DrupalDropzoneTest.php

--- tests/Integration/DrupalDropzoneTest.php
+++ tests/Integration/DrupalDropzoneTest.php
@@ -7,7 +7,7 @@ class DrupalDropzoneTest extends IntegrationBase
     protected $package = 'drupal/dropzonejs';
     protected $directory = 'drupal-dropzonejs';
 
-    public function testEndToEnd() 
+    public function testEndToEnd()
     {
         if (getenv('COMPOSER_VERSION') == 2) {
             self::assertTrue(true, 'Skipping test on composer version 2');
