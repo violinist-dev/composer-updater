4beaadf5660705b92308c20c46783d4f524e1216
Eirik Stanghelle Morland <eirik@morland.no>
2022-04-17 08:34:54 +0200

    Update DrupalCommerceTest.php

--- tests/Integration/DrupalCommerceTest.php
+++ tests/Integration/DrupalCommerceTest.php
@@ -7,7 +7,7 @@ class DrupalCommerceTest extends IntegrationBase
     protected $package = 'drupal/commerce';
     protected $directory = 'drupal-commerce';
 
-    public function testEndToEnd() 
+    public function testEndToEnd()
     {
         if (getenv('COMPOSER_VERSION') == 2) {
             self::assertTrue(true, 'Skipping test on composer version 2');
