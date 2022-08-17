bd52a8c8c383b280159dbfccf9e27adc5ab81fc3
Eirik Stanghelle Morland <eirik@morland.no>
2022-08-17 20:11:20 +0200

    Update DrupalTest.php

--- tests/Integration/DrupalTest.php
+++ tests/Integration/DrupalTest.php
@@ -13,6 +13,10 @@ class DrupalTest extends IntegrationBase
             self::assertTrue(true, 'Skipping test on composer version 2');
             return;
         }
+        if (version_compare(phpversion(), "8.0.0", "<=")) {
+            self::assertTrue(true, sprintf('Skipping integration test %s for version %s since PHP version is more than 8.0.0', get_class($this), phpversion()));
+            return;
+        }
         parent::testEndToEnd();
     }
 }
