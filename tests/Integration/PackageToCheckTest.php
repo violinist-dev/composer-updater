19c47936364da72a1e90185fbe8038aac7b8aa05
Eirik S. Morland <eirik@morland.no>
2023-10-16 22:10:16 +0200

    Add a test for package to check

---
+++ tests/Integration/PackageToCheckTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Violinist\ComposerUpdater\Tests\Integration;
+
+use Violinist\ComposerUpdater\Updater;
+
+class PackageToCheckTest extends IntegrationBase
+{
+    protected $package = 'drupal/captcha';
+    protected $directory = 'drupal-captcha';
+
+    protected function createUpdater($directory)
+    {
+        $updater = new Updater($directory, 'drupal/recaptcha');
+        $updater->setPackageToCheckHasUpdated($this->package);
+        return $updater;
+    }
+
+}
