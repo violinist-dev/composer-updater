ad44cf14423afaf558d7865e37e25e1e2e5add8b
Eirik S. Morland <eirik@morland.no>
2023-10-17 10:13:07 +0200

    Allow fail on 7.2

--- tests/Integration/RequireTest.php
+++ tests/Integration/RequireTest.php
@@ -2,10 +2,21 @@
 
 namespace Violinist\ComposerUpdater\Tests\Integration;
 
+use Violinist\ComposerUpdater\Exception\NotUpdatedException;
+
 class RequireTest extends IntegrationBase
 {
     protected $package = 'drupal/captcha';
     protected $directory = 'drupal-captcha';
     protected $useUpdate = false;
     protected $newVersion = '2.0.5';
+
+    public function testEndToEnd()
+    {
+        if (version_compare(phpversion(), "7.3.0", "<")) {
+            $this->expectException(NotUpdatedException::class);
+        }
+        parent::testEndToEnd();
+    }
+
 }
