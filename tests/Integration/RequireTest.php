189f76cd41b9f157ce48ee104673a8465e0ddb7d
Eirik Stanghelle Morland <eirik@morland.no>
2023-10-19 14:32:40 +0200

    Add a require test as well (#44)

---
+++ tests/Integration/RequireTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Violinist\ComposerUpdater\Tests\Integration;
+
+use Violinist\ComposerUpdater\Exception\NotUpdatedException;
+
+class RequireTest extends IntegrationBase
+{
+    protected $package = 'drupal/captcha';
+    protected $directory = 'drupal-captcha';
+    protected $useUpdate = false;
+    protected $newVersion = '2.0.5';
+
+    public function testEndToEnd()
+    {
+        if (version_compare(phpversion(), "8.0.0", "<")) {
+            $this->expectException(NotUpdatedException::class);
+        }
+        parent::testEndToEnd();
+    }
+}
