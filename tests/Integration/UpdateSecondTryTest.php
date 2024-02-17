0481eb120142925b01dfe5b4d20097cc72615978
Eirik S. Morland <eirik@morland.no>
2024-02-17 13:35:03 +0100

    Also add a test to make sure it works

---
+++ tests/Integration/UpdateSecondTryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Integration;
+
+use Violinist\ComposerUpdater\Exception\NotUpdatedException;
+use Violinist\ComposerUpdater\Tests\Integration\IntegrationBase;
+
+class UpdateSecondTryTest extends IntegrationBase
+{
+    protected $package = 'drupal/captcha';
+    protected $directory = 'drupal-captcha';
+    protected $newVersion = '2.0.5';
+
+    public function testEndToEnd()
+    {
+        if (version_compare(phpversion(), "8.0.0", "<")) {
+            $this->expectException(NotUpdatedException::class);
+        }
+        parent::testEndToEnd();
+    }
+
+    protected function createUpdater($directory)
+    {
+        $updater = parent::createUpdater($directory);
+        $updater->setPackagesToCheckHasUpdated(['doctrine/annotations', 'drupal/captcha']);
+        return $updater;
+    }
+}
